eccaeb57c290894aac49bf560d2631074eaddd6d
pramod-wdo <pramodhc007@gmail.com>
2022-11-01 19:58:18 +0530

    adds properties feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_01_132535) do
+ActiveRecord::Schema.define(version: 2022_11_01_133429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -27,6 +27,19 @@ ActiveRecord::Schema.define(version: 2022_11_01_132535) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.bigint "account_id"
+    t.string "name"
+    t.string "address"
+    t.decimal "price"
+    t.integer "rooms"
+    t.integer "bathrooms"
+    t.string "photo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
 end
